06fe19079097e896840a6968b433793ff9872536
MiltonNji <112828503+MiltonNji@users.noreply.github.com>
2026-01-21 01:11:58 +0000

    commit

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -3,6 +3,7 @@
 
 
 from sys import prefix
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
@@ -41,18 +42,17 @@ parse_dates = [
 
 
 
-def run():
-  pg_user = 'root'
-  pg_pass = 'root'
-  pg_host = 'localhost'
-  pg_port = '5432'
-  pg_db = 'ny_taxi'
-
-  year = 2021
-  month = 1
-
-  target_table = 'yellow_taxi_data'
-  chunksize = 100000
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default='5432', help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', type=int, default=2021, help='Year of data to ingest')
+@click.option('--month', type=int, default=1, help='Month of data to ingest')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+@click.option('--chunksize', type=int, default=100000, help='Chunk size for reading data')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
 
 
   prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
